9910d9a45ec122c1f847d40c0a7f5e12a0e5ebbc
YurGoV <yurgov@gmail.com>
2023-01-15 00:14:56 +0200

    userMenu/added

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -3,33 +3,29 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
+// import Button from '@mui/material/Button';
 import {HomeLinkStyled, RegisterButtonStyled} from "./Navigation.styled";
 // import {NavLink} from "react-router-dom";
 // import IconButton from '@mui/material/IconButton';
 // import MenuIcon from '@mui/icons-material/Menu';
 // import {selectIsLoggedIn} from "../../redux/selectors";
-import {useDispatch, useSelector} from "react-redux";
+import {useSelector} from "react-redux";
 import {selectIsLogged} from "../../redux/selectors";
-import {logoutUser} from "../../redux/authOperations";
+// import {logoutUser} from "../../redux/authOperations";
+import UserMenu from "../UserMenu/UserMenu";
 
 export default function ButtonAppBar() {
-  const dispatch = useDispatch();
+  // const dispatch = useDispatch();
 
   const isLogged = useSelector(selectIsLogged);
   console.log(isLogged);
 
 
-  const logout = () => {
-    console.log('lllll');
-    dispatch(logoutUser());
-  }
-
-  return (
+   return (
     <Box sx={{flexGrow: 1}}>
 
       <AppBar position="static" sx={{display: 'flex', alignItems: 'center', minWidth: '100wh'}}>
-        <Box minWidth='600px'>
+        <Box minWidth='800px'>
           <Toolbar sx={{justifyContent: 'space-between'}}>
             {/*<IconButton
             size="large"
@@ -53,14 +49,14 @@ export default function ButtonAppBar() {
                 <RegisterButtonStyled to={'/register'}>
                   SIGNUP
                 </RegisterButtonStyled> </Box>
-              : <Box sx={{display: 'flex', flexDirection: 'row', justifyContent: 'center'}}>
+              : <Box sx={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between', width: '70%'}}>
 
                 <HomeLinkStyled to={'/contacts'}>
                   <Typography  >
                     CONTACTS
                   </Typography> </HomeLinkStyled>
 
-                <Button onClick={() => logout()} color="inherit">LogOut</Button>
+                <UserMenu></UserMenu>
               </Box>
             }
 
